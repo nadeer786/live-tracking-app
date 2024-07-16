a2da49b5d9203b255e1c1c99f263ddd5c185b42d
nadeer786 <muhammednadeer101@gmail.com>
2024-07-16 14:59:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("sendLocation", (data) => {
+    console.log(data);
+    io.emit("receiveLocation", { id: data.id, ...data });
+  });
+  console.log("a user connected");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      socket.emit("sendLocation", {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    },
+    (err) => {
+      console.log(err);
+    },
+    { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+  );
+}
+
+// L.map("map", {
+//   center: [0, 0],
+//   zoom: 2,
+// });
+
+const map = L.map("map").setView([0, 0], 3);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Calicut, India",
+}).addTo(map);
+
+const marker = {};
+
+socket.on("receiveLocation", (data) => {
+  console.log(data);
+  const { id, latitude, longitude } = data;
+
+  map.setView([latitude, longitude], 16);
+
+  if (!marker[id]) {
+    marker[id] = L.marker([latitude, longitude]).addTo(map);
+  } else {
+    marker[id].setLatLng([latitude, longitude]);
+  }
+});
